f03e95d3bf81102b04cf2b0001cc068552386834
ovaldez <fabrogt0052@gmail.com>
2021-10-14 15:31:34 -0500

    Se reescribe el inicio para poder tenerlo en JAde

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', color: 'blue' });
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express', color: 'blue' });
 });
 
 router.get('/pagina', function(req, res, next) {
