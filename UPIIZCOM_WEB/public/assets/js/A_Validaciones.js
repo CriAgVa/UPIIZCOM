0be13b67d47d27cab59a69e5a5a1c23f21028f31
rubenespitia <ruben.0519@hotmail.com>
2021-11-12 13:10:28 -0600

    Intento De Validaciones

---
+++ UPIIZCOM_WEB/public/assets/js/A_Validaciones.js
@@ -0,0 +1,15 @@
+
+
+(function(){
+
+    var app = angular.module('myApp', []);
+
+    app.controller('validateCtrl', function($scope) {
+        $scope.usuario = '2019670005';
+        $scope.password = 'Norecuerdo';
+
+
+        
+    });
+
+})();
