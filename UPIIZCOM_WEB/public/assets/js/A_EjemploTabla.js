6a87499bd94a3487ceed68b4e098f0c984bba130
rubenespitia <ruben.0519@hotmail.com>
2022-02-26 17:44:50 -0600

    Se tiene de forma completa las notificaciones por parte web, teniendo en 2 funciones la accion de suscribirse y de mandar una notificacion personalizada, estas funciones se alojan dentro de A_EjemploTabla y se usa de forma momentanea la pagina del FAQ como recurso para acceder a las pruebas unitarias de estas 2 funciones. Se usa el puerto localhost 3001 para las pruebas de forma local...

--- UPIIZCOM_WEB/public/assets/js/A_EjemploTabla.js
+++ UPIIZCOM_WEB/public/assets/js/A_EjemploTabla.js
@@ -11,6 +11,91 @@
         $scope.integrantes;
         $scope.miembros = [];
 
+        //////////////////////////////////////////////////////////Notificaciones funciones
+        $scope.successSubscription = async function(){
+            //Las llaves se generan una vez por proyecto, intentar no mover que no sé desuscribir
+            const PUBLIC_VAPID_KEY = "BAJsV1r7TlO6xgRYruDPqcZaXg1k0kY56bVnymq4uUg9Gsf7XS6qYYjtisRDcFhhJWBmBVJd9fkOnDGjSrqpgFM";
+            //funcion para construccion del mensaje
+            function urlBase64ToUint8Array(base64String) {
+            const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+            const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+            const rawData = window.atob(base64);
+            const outputArray = new Uint8Array(rawData.length);
+
+            for (let i = 0; i < rawData.length; ++i) {
+                outputArray[i] = rawData.charCodeAt(i);
+            }
+            return outputArray;
+            }
+            /////////////////////////////Inicio de la funcion de suscripcion persé
+                // Service Worker
+                console.log("Registering a Service worker");
+                const register = await navigator.serviceWorker.register("/worker.js", {
+                scope: "/"
+                });
+                console.log("New Service Worker");
+
+                // Listener de Push Notifications
+                console.log("Listening Push Notifications");
+                const subscription = await register.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(PUBLIC_VAPID_KEY)
+                });
+
+                console.log(subscription);
+
+                // Maanda la subscripcion 
+                await fetch("/subscription", {
+                method: "POST",
+                body: JSON.stringify(subscription),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+                });
+                console.log("Subscribed!");
+
+                await fetch('/new-message', {
+                method: 'POST',
+                body: JSON.stringify({message: 'Prueba de suscripcion', title:'Estás suscrito' }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+                });
+                //console.log('Si llego jsjs'); Monitoreo de consola si llega la subs
+
+                
+            // Detectamos errores
+            if ("serviceWorker" in navigator) {
+                subscription().catch(err => console.log(err));
+            }
+  
+         }
+
+        $scope.showNotification = async function(){
+           // $window.location.href = '/grupos'; nos regresa a una referencia anterior
+            //const form = document.querySelector('#myform'); resetea la forma donde está puesta en html
+            const message = document.querySelector('#message');//Importante que se llame igual esta variable en el jade y aqui
+           // const form2 = document.querySelector('#myformT'); resetea la forma donde está puesta html
+            const title = document.querySelector('#title');//Importante que se llame igual esta variable en el jade y aqui
+            await fetch('/new-message', {
+                method: 'POST',
+                //body: JSON.stringify({message: 'Funcion en botón', title:'YesYesYes' }),//Mensaje forzado
+                body: JSON.stringify({message: message.value, title:title.value }),//Mensaje adquirido
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              });
+              //console.log('Si llego jsjs'); Mostrar en consola si llega
+              
+            // Detectamos errores
+            if ("serviceWorker" in navigator) {
+                subscription().catch(err => console.log(err));
+            }
+            
+        }
+///////////////////////////////////////////////////Fin de funciones de notificaciones
+
         $scope.ocultaMuestra = function(){
             $scope.mostrar = ( $scope.mostrar == 1 ) ? 0 : 1;
         }
